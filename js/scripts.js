bcdc00ec49604854fd42a4d6a4614abaa68f2734
Ivan Ramos <Ivanramos148248@gmail.com>
2018-01-12 08:10:21 -0800

    added blue background and centered my text

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
           var passCode = $("input#passCode").val();
 
-          if (passCode === 'ivy') {
-            $('#uwu').show();
-          }
+      if (passCode === 'password') {
+          $('#hide').show();
+              } else {
+        alert('sorry try again!')
+      }
+      
 
       event.preventDefault();
     });
